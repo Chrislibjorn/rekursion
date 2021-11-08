eff51a30b49ac5437f0f07c0ce5ad3ca8f80a65c
Chrislibjorn <christofferholstthomsen@gmail.com>
2021-11-08 10:13:37 +0100

    Added changable layers

--- Program.cs
+++ Program.cs
@@ -6,22 +6,31 @@ namespace rekursion
     {
         static void Main(string[] args)
         {
-            hanoi(4,"A","B","C");
+            int number;
+            bool isParsable = Int32.TryParse(args[0],out number);
+            if(isParsable)
+            hanoi(number, "A", "B", "C");
+            else
+            {
+                Console.WriteLine("use a whole number");
+            }
 
 
-            void move(string f,string t)
+            void move(string f, string t)
             {
                 Console.WriteLine($"move disc from {f} to {t}!");
             }
-            void hanoi(int n, string f,string h, string t)
+            void hanoi(int n, string f, string h, string t)
             {
-                if(n==0){
+                if (n == 0)
+                {
                     return;
                 }
-                else{
-                hanoi(n-1,f,t,h);
-                move(f,t);
-                hanoi(n-1,h,f,t);
+                else
+                {
+                    hanoi(n - 1, f, t, h);
+                    move(f, t);
+                    hanoi(n - 1, h, f, t);
                 }
             }
         }
